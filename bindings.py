897a41741b2cb56cfe41294872a1f93f3fd5ab03
SamuelFe <54598690+SamuelFe@users.noreply.github.com>
2022-06-16 14:13:39 -0300

    bingings -> bindings.py

--- bindings.py
+++ bindings.py
@@ -6,4 +6,3 @@ config.bind('A', 'fake-key <End>;; mode-enter insert')
 config.bind('cw', 'fake-key <Ctrl-Delete>;; mode-enter insert')
 config.bind('cc', 'fake-key <Home><Shift-End><Delete>;; mode-enter insert')
 config.bind('c$', 'fake-key <Shift-End><Delete>;; mode-enter insert')
-
